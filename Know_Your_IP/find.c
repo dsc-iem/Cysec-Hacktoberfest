e962b729e8ba4b6ec37297c0190b2dd1d28c8d08
Divya Prakash <divya18.prak@gmail.com>
2020-09-18 17:44:22 +0530

    first commit

---
+++ Know_Your_IP/find.c
@@ -0,0 +1,41 @@
+// C program to determine class, Network , direct broadcast address
+// and Host ID of an IPv4 address 
+#include<stdio.h> 
+#include<string.h> 
+
+// Function to find out the Class 
+char findClass(char str[]) 
+{ 
+	//complete the function to find the class of the given IPv4 address. Consult README.md to know more about a class of an ip address. 
+} 
+
+// Function to separate Network ID as well as 
+// Host ID and print them 
+void separate(char str[], char ipClass) 
+{ 
+
+	//Complete the finction for classes A, B and C to find the network ID and host Id and print the same. Use switch case
+
+	// Class D and E are not divided in Network 
+	// and Host ID 
+	//provide the case for same and print "cannot be divided"
+} 
+
+void subnet(char ipclass){
+
+	//complete the function to find subnet mask 
+	// of the given IPv4 address using the class.
+}
+
+// main function
+int main() 
+{ 
+	char str[20] ;
+	printf("Enter IP address \n");
+	fgets(str, 20, stdin); 
+	char ipClass = findClass(str); 
+	subnet(ipClass)
+	printf("Given IP address belongs to Class %c\n", ipClass); 
+	separate(str, ipClass); 
+	return 0; 
+} 
---
+++ README.md
@@ -0,0 +1,20 @@
+
+<img src="assets/logo.svg" alt="dsc logo" width="500"/>
+
+
+# Cybersecurity Hacktoberfest repository 
+
+**This repository contains two very basic  programmes one of which is focused on ``networking`` while other on </br> a very beginner yet very important vulnerability ``information gathering``**
+
+</br>
+
+***Key Logger***
+
+`Key logger, often referred to as keystroke logger or keyboard capture, is the action of recording the keys struck on a keyboard, typically covertly, so that person using the keyboard is unaware that their actions are being monitored. A key logger can be a software or a physical device we will be making one such software in this directory.`
+
+
+***Know_Your_IP***
+
+`IPv4 addresses are 32-bit numbers that are typically displayed in dotted decimal notation. A 32-bit address contains two primary parts: the network prefix and the host number. All hosts within a single network share the same network address. Each host also has an address that uniquely identifies it. IPv4 addresses are also classified into 5 different classes.`
+
+### Head over to the directories to know more
